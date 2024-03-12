3cd613ed264346150269e7a32908893d0bff43c5
JaoodxD <jaood96@gmail.com>
2024-03-12 12:37:29 +0200

    Add test iteraction with mariadb

--- fastify-server-1/index.js
+++ fastify-server-1/index.js
@@ -1,18 +1,32 @@
 const Fastify = require('fastify')
+const mysql = require('mysql2/promise')
 
 const host = process.env.host || '0.0.0.0'
 const port = process.env.port || 3000
+const MYSQL_USER = process.env.MYSQL_USER
+const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD
 
 const app = Fastify()
 
-app.get('/', (req, res) => {
-  console.log(`handling request on port ${port}`)
-  res.send({ hello: 'world' })
-})
-
 start()
 
-async function start () {
+async function start() {
+  const conn = await mysql.createConnection({
+    host: 'mariadb',
+    user: MYSQL_USER,
+    password: MYSQL_PASSWORD
+  })
+
+  await conn.connect()
+  const [result] = await conn.query('SELECT 1 + 2 AS TOTAL')
+  console.log('test mariadb response:', result)
+
+  app.get('/', async(req, res) => {
+    console.log(`handling request on port ${port}`)
+    const [result] = await conn.query('SELECT 1 + 2 AS TOTAL')
+    return result
+    // res.send({ hello: 'world' })
+  })
   try {
     await app.listen({ host, port })
     console.log(`Server listen on port ${port}`)
